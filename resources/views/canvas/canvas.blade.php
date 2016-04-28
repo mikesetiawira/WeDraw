b6689e58fad135485c9b37a1f0fbd0b145f6764b
Siti Fatimah <ifanasz@gmail.com>
2016-04-29 03:48:45 +0700

    gallery, room, canvas edit

--- resources/views/canvas/canvas.blade.php
+++ resources/views/canvas/canvas.blade.php
@@ -7,59 +7,56 @@
 @section('content')
 <div class="content">
   <img id="crayons" src="crayontexture.png" hidden> 
-  <div class="col-md-6 col-md-offset-3"> 
-  <table>
-    <tr>
-      <td>Canvas size :  </td>
-      <td><input id="width" value="800"> x <input id="height" value="480"> px</td>
-    </tr>
+
+  <h3>Image Title</h3>
+  <h4>Image Owner</h4>
+  
+  <p>Canvas size :  <input id="width" value="800"> x <input id="height" value="480"> px</p>
+    
+  <div class="sketch" oncontextmenu="return false">
+    <canvas id="paint" width="800" height="480" style="border:1px solid #787272;"></canvas>
+  </div>
+
+  <div class="drawing-tools">
+    <table>
     <tr>
       <td>Color :  </td>
-      <td><input id="color" class="jscolor" value="ab2567"></td>
-    </tr>
-    <tr>
-      <td>Size :  </td>
-      <td><input id="size" value="10"></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>
-        <button id="a" class="btn-tools">Normal</button> 
-        <button id="b" class="btn-tools">Large</button> 
-        <button id="c" class="btn-tools">Huge</button>
-      </td>
-    </tr>
-    <tr>
+      <td class="val"><input id="color" class="jscolor" value="ab2567"></td>
+      
       <td>Environment :  </td>
-      <td>
+      <td class="val">
         <button id="AA" class="btn-tools">Clean</button> 
         <button id="BB" class="btn-tools">Crayon</button>
       </td>
+
+      <td><button id="clear" class="btn-clear">Clear</button></td>
     </tr>
     <tr>
+      <td>Size :  </td>
+      <td class="val"><input id="size" value="10"></td>
+
       <td>Tools :  </td>
-      <td>
+      <td class="val">
         <button id="A" class="btn-tools">Eraser</button> 
         <button id="B" class="btn-tools">Marker</button>
       </td>
     </tr>
     <tr>
+      <td></td>
+      <td class="val">
+        <button id="a" class="btn-tools">Normal</button> 
+        <button id="b" class="btn-tools">Large</button> 
+        <button id="c" class="btn-tools">Huge</button>
+      </td>
+
       <td>Shapes :  </td>
-      <td>
+      <td class="val">
         <button id="M" class="btn-tools">Line</button>
         <button id="O" class="btn-tools">Square</button>
         <button id="N" class="btn-tools">Ellipse</button>
       </td>
     </tr>
-    <tr>
-      <td></td>
-      <td><button id="clear" class="btn-clear">Clear</button></td>
-    </tr>
-  </table>
-  </div>
-    
-  <div class="sketch" oncontextmenu="return false">
-    <canvas id="paint" width="800" height="480" style="border:1px solid #787272;"></canvas>
+    </table>
   </div>
 
   <script src="js/jquery-1.12.2.min.js"></script>
--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -6,6 +6,83 @@
 
 @section('content')
 <div class="content">
-    <h3>GALLERY</h3>
+  <h3>GALLERY</h3>
+
+  <div class="gallery">
+  	<table>
+  	  <tr>
+  	  	<td><a href=#>
+  	  	  <img onclick="myFunction(this.id)" data-toggle="modal" data-target="#myModal" id="image" src="http://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/256/blue-user-icon.png">
+  	  	</a></td>
+  	  	<td><img src="http://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/256/blue-user-icon.png"></td>
+  	  	<td><img src="http://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/256/blue-user-icon.png"></td>
+  	  </tr>
+
+  	  <tr>
+  	  	<td class="title">Image Title</td>
+  	  	<td class="title">Image Title</td>
+  	  	<td class="title">Image Title</td>
+  	  </tr>
+
+  	  <tr class="owner">
+  	  	<td>Image Owner</td>
+  	  	<td>Image Owner</td>
+  	  	<td>Image Owner</td>
+  	  </tr>
+
+
+  	  <tr>
+  	  	<td><img src="http://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/256/blue-user-icon.png"></td>
+  	  	<td><img src="http://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/256/blue-user-icon.png"></td>
+  	  	<td><img src="http://icons.iconarchive.com/icons/double-j-design/origami-colored-pencil/256/blue-user-icon.png"></td>
+  	  </tr>
+
+  	  <tr>
+  	  	<td class="title">Image Title</td>
+  	  	<td class="title">Image Title</td>
+  	  	<td class="title">Image Title</td>
+  	  </tr>
+
+  	  <tr class="owner">
+  	  	<td>Image Owner</td>
+  	  	<td>Image Owner</td>
+  	  	<td>Image Owner</td>
+  	  </tr>
+  	</table>
+
+  	<!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Image Title</h4>
+          </div>
+          
+          <div class="modal-body">
+            <img src="" id="modal-image">
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--END MODAL-->
+  </div>
+
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script>
+    $(document).ready(function(){
+    	$('[data-toggle="tooltip"]').tooltip();
+    });
+  </script>
+  <script>
+  function myFunction(clicked_id) {
+    q = document.getElementById(clicked_id).src;
+    document.getElementById('modal-image').src=q;
+  }
+  </script>
 </div>
 @endsection
